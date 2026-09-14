2bc5102a7521db233f7b31bf9c7b20fbe625841d
Bence20091122 <kovacsb394@gmail.com>
2026-09-14 19:01:52 +0200

    Python rész kész! 👌👌👌👍

--- nepesseg.py
+++ nepesseg.py
@@ -1,4 +1,8 @@
 nepesseg = []
+telepulesek_szama = 0
+adott_megyek = []
+osszlakosok = 0
+ossz_varos_lakok = 0
 
 with open("lakossag_2025.csv", "r", encoding="UTF-8") as forrasfajl:
     forrasfajl.readline()
@@ -10,37 +14,112 @@ with open("lakossag_2025.csv", "r", encoding="UTF-8") as forrasfajl:
             "megye": adatok[0],
             "telepules": adatok[1],
             "tipus": adatok[2],
-            "ferfi": adatok[3].replace(" ", ""),
-            "no": adatok[4].replace(" ", "")
+            "ferfi": int(adatok[3].replace(" ", "")),
+            "no": int(adatok[4].replace(" ", ""))
         }
 
         nepesseg.append(n)
 
+while True:
 
-telepules = int(input(
-    "[1] Nyomja meg az 1-es gombot a megye adatai lekéréséhez\n"
-    "[2] Nyomja meg a 2-es gombot a település típusainak lekéréséhez: "
-))
+    telepules = input(
+        "[1] Megye adatai\n"
+        "[2] Település típusok\n"
+        "[X] Kilépés\n"
+        "\nVálasztás: "
+    ).lower()
 
-telepulesek_szama = 0
-adott_megyek = []
-osszlakosok = 0
-ossz_varos_lakok = 0
+    if telepules == "1":
 
-if telepules == 1:
-    bekert_megye = input("Adjon meg egy megyét (PL: HAJ): ")
+        bekert_megye = input("Adjon meg egy megyét (PL: HAJ): ").upper()
 
-    for i in nepesseg:
-        if i["megye"].lower() == bekert_megye.lower():
-            telepulesek_szama += 1
-            adott_megyek.append(i)
-    for i in adott_megyek:
-        x = int(i["ferfi"]) + int(i["no"])
-        osszlakosok += x
-        if i["tipus"] == "város":
+        for i in nepesseg:
+            if i["megye"] == bekert_megye:
+                telepulesek_szama += 1
+                adott_megyek.append(i)
+        for i in adott_megyek:
             x = int(i["ferfi"]) + int(i["no"])
-            ossz_varos_lakok += x
+            osszlakosok += x
+            if i["tipus"] == "város":
+                x = int(i["ferfi"]) + int(i["no"])
+                ossz_varos_lakok += x
 
-print(f"Ebben a megyében {telepulesek_szama} település található.")
-print(f"{osszlakosok} lakik itt.")
-print(f"{ossz_varos_lakok} ember lakik ebből városokban.")
+        print(f"Ebben a megyében {telepulesek_szama} település található.")
+        print(f"{osszlakosok} lakik itt.")
+        print(f"{ossz_varos_lakok} ember lakik ebből városokban.")
+
+    elif telepules == "2":
+
+        tipusok = []
+
+        for i in nepesseg:
+            if i["tipus"] not in tipusok:
+                tipusok.append(i["tipus"])
+
+
+        print("\n--- Településtípusok ---")
+
+        for sorszam, tipus in enumerate(tipusok, 1):
+            print(f"[{sorszam}] {tipus}")
+
+        valasztas = int(input("\nVálasszon egy településtípust: "))
+
+        bekert_tipus = tipusok[valasztas - 1]
+
+        kozseg_tipusok = []
+
+        for i in nepesseg:
+            if i["tipus"] == bekert_tipus:
+                kozseg_tipusok.append(i)
+
+        oldal = 0
+        oldal_meret = 10
+
+        while True:
+
+            kezdet = oldal * oldal_meret
+            veg = kezdet + oldal_meret
+
+            print("\n--- Települések ---")
+
+            for i in kozseg_tipusok[kezdet:veg]:
+
+                lakossag = i["ferfi"] + i["no"]
+
+                print(f'{i["telepules"]} - {lakossag} fő')
+
+
+            print(
+                f"\nOldal: {oldal + 1} / "
+                f"{(len(kozseg_tipusok) + oldal_meret - 1) // oldal_meret}"
+            )
+
+            print("\n[N] Következő oldal")
+            print("[B] Előző oldal")
+            print("[Q] Vissza a főmenübe")
+
+            valasztas = input("Választás: ").lower()
+
+
+            if valasztas == "n".lower():
+
+                if veg < len(kozseg_tipusok):
+                    oldal += 1
+                else:
+                    print("Ez az utolsó oldal!")
+
+            elif valasztas == "b".lower():
+
+                if oldal > 0:
+                    oldal -= 1
+                else:
+                    print("Ez az első oldal!")
+
+
+            elif valasztas == "q".lower():
+                break      
+
+    elif telepules == "x".lower():
+
+        print("Program vége!")
+        break              
