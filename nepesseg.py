5399ebe73a009140b9aa1bfe3bfe68b1411862c1
boying1819 <szabobence090608@gmail.com>
2026-09-14 18:45:31 +0200

    1.-es feladat és szep printek megoldása.

--- nepesseg.py
+++ nepesseg.py
@@ -10,8 +10,8 @@ with open("lakossag_2025.csv", "r", encoding="UTF-8") as forrasfajl:
             "megye": adatok[0],
             "telepules": adatok[1],
             "tipus": adatok[2],
-            "ferfi": adatok[3],
-            "no": adatok[4]
+            "ferfi": adatok[3].replace(" ", ""),
+            "no": adatok[4].replace(" ", "")
         }
 
         nepesseg.append(n)
@@ -23,12 +23,24 @@ telepules = int(input(
 ))
 
 telepulesek_szama = 0
+adott_megyek = []
+osszlakosok = 0
+ossz_varos_lakok = 0
 
 if telepules == 1:
     bekert_megye = input("Adjon meg egy megyét (PL: HAJ): ")
 
     for i in nepesseg:
-        if i["megye"] == bekert_megye:
+        if i["megye"].lower() == bekert_megye.lower():
             telepulesek_szama += 1
+            adott_megyek.append(i)
+    for i in adott_megyek:
+        x = int(i["ferfi"]) + int(i["no"])
+        osszlakosok += x
+        if i["tipus"] == "város":
+            x = int(i["ferfi"]) + int(i["no"])
+            ossz_varos_lakok += x
 
-print(telepulesek_szama)
+print(f"Ebben a megyében {telepulesek_szama} település található.")
+print(f"{osszlakosok} lakik itt.")
+print(f"{ossz_varos_lakok} ember lakik ebből városokban.")
